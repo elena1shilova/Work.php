9c65af06d8c202345c61ebd4f89666ec28512f61
elena1989 <elena.shilova89@gmail.com>
2022-07-12 21:07:57 +0300

    new1

--- index.php
+++ index.php
@@ -1,59 +1,60 @@
-<!-- <?php
-$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
+<?php
+$text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua';
 echo $nev_text = str_replace (',','',$text), '<br>';//замена запятых пробелами
 $arr = explode (' ', ($nev_text));//приобразуем в массив
-$min = $arr[0];
+//$min = $arr[0];
 
 // print_r($arr);
-$max = strlen($arr[0]);
-$min = strlen($arr[0]);
-foreach($arr as $i=>$key)
-{
- 
-if($max < strlen($key)){
-    $max = $key;
-    $valueMax = $i;
-}
-if($min > strlen($key)){
-    $min = $key;
-    $valueMin = $i;
-}
+$maxLenght = strlen($arr[0]);
+$maxWord = '';
+$minLenght = strlen($arr[0]);
+$minWord = '';
+foreach($arr as $key=>$valuo){
 
+    if($maxLenght < strlen($valuo)){
+        $maxLenght = strlen($valuo);
+        $maxWord = $valuo;
+        $indexMax = $key;
+    }
+//    var_dump($max);
+//    var_dump(strlen($valuo));
+//    die();
+    if($minLenght > strlen($valuo)){
+        $minLenght = strlen($valuo);
+        $minWord = $valuo;
+        $indexMin = $key;
+    }
+
+//    echo '<pre>';
+//    var_dump($min);
+//    echo '</pre>';
 }
 
 echo 'Максимальное значение массива и его Индекс<br>';
-echo $max.'<br>';
-echo $valueMax.'<br>';
+echo $maxWord.'<br>';
+echo $indexMax.'<br>';
 echo 'Минимальное значение массива и его Индекс<br>';
-echo $min.'<br>';
-echo $valueMin;
-?> -->
-<!-- 
-//     for ($i=0; $i < count($arr); $i++) { 
-//          if((strlen($arr[$i]))>(strlen($max))) $max = $arr[$i];
-//     }
-//     for ($i=0; $i < count($arr); $i++) { 
-//         if((strlen($arr[$i]))<(strlen($min))) $min = $arr[$i];
-//    }
-// $arr[$value] = strlen($value);
-// if ($arr[$value]>$max) 
-// {
-// echo $arr[$value];
-// }
--->
+echo $minWord.'<br>';
+echo $indexMin;
+?>
 
-<!-- 
 
+<!---->
 <?php
-$userAge = array('Антон' => 13,
-                'Денис' => 82,
-                'Дима' => 18,
-                'Анна' => 24,
-                'Ольга' => 36);
-foreach ($userAge as $key => $value) {
-    if($value >= 18) echo $key . ' - Привет! <br>';
-    else echo $key . ' - Вам сюда нельзя! <br>';
-}
-?> -->
+//$userAge =      ['Антон' => 13,
+//                'Денис' => 82,
+//                'Дима' => 18,
+//                'Анна' => 24,
+//                'Ольга' => 36];
+//foreach ($userAge as $key => $value) {
+//    if($value >= 18) {
+//        echo $key . ' - Привет! <br>';
+//    }
+//    else {
+//        echo $key . ' - Вам сюда нельзя! <br>';
+//    }
+//}
+//?>
 
 
